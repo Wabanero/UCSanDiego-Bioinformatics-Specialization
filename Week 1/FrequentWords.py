ff43657fa31ff7a619e147ed3dacd2b92f13a8f3
Wabanero <46268971+Wabanero@users.noreply.github.com>
2019-05-16 00:29:15 +0200

    Create FrequentWords.py

---
+++ Week 1/FrequentWords.py
@@ -0,0 +1,7 @@
+"""FrequentWords.py:
+        Code Challenge, implement PatternCount algorithm(reproduced below).
+        Input: Strings Text and Pattern.
+        Output: Count(Text, Pattern)."""
+
+__author__      = "Filippo Bergeretti"
+__date__        = "14 May 2019"
